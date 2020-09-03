4128d02ce361af1a0b5d84315ae7cf19b880a757
Henrik Libeck Hexeberg <henrik.lh@hotmail.com>
2020-09-03 13:14:01 +0200

    change System.out and Sustem.err to logger in Server.java

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -4,16 +4,20 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Server extends Thread{
+public class Server extends Thread {
 
     private final static String HOST_ADDRESS = "localhost";
     private final static int PORT = 6666;
     private ServerSocket serverSocket;
+    private Logger logger;
 
-    public Server(){
+    public Server() {
         this.serverSocket = createServerSocket();
         bindServerSocket(this.serverSocket, this.HOST_ADDRESS, this.PORT);
+        this.logger = Logger.getLogger(Server.class.getName());
     }
 
     public String getHostAddress() {
@@ -29,7 +33,7 @@ public class Server extends Thread{
         try {
             newServerSocket = new ServerSocket();
         } catch (IOException e) {
-            System.err.println("Error occurred trying to create a ServerSocket");
+            this.logger.log(Level.SEVERE, "Error occurred trying to create a ServerSocket");
             e.printStackTrace();
         }
         return newServerSocket;
@@ -38,9 +42,11 @@ public class Server extends Thread{
     private void bindServerSocket(ServerSocket serverSocket, String hostToBeBound, int portToBeBound) {
         try {
             serverSocket.bind(new InetSocketAddress(hostToBeBound, portToBeBound));
-            System.out.println("Server running on: " + serverSocket.getLocalSocketAddress());
+            this.logger.log(Level.INFO, "Server running on: " + serverSocket.getLocalSocketAddress());
         } catch (IOException e) {
-            System.err.printf("Could not bind server to address: %s and port %d", hostToBeBound, portToBeBound);
+            this.logger.log(Level.SEVERE,
+                    "Could not bind server to address: " + hostToBeBound + " and port " + portToBeBound
+            );
             e.printStackTrace();
         }
     }
@@ -50,7 +56,7 @@ public class Server extends Thread{
         while (true) {
             try {
                 Socket firstClient = serverSocket.accept();//establishes connection
-                System.out.println("connection established with " + firstClient.getInetAddress());
+                this.logger.log(Level.INFO,"connection established with " + firstClient.getInetAddress());
                 return;
             } catch (Exception e) {
                 e.printStackTrace();
