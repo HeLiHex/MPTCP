fd175c5df3b642daa7689465761319c357c4c975
Henrik Libeck Hexeberg <henrik.lh@hotmail.com>
2020-10-06 13:47:00 +0200

    Buffer queue is no longer generic and should contain Packets

--- src/main/java/org/example/data/BufferQueue.java
+++ src/main/java/org/example/data/BufferQueue.java
@@ -2,7 +2,7 @@ package org.example.data;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class BufferQueue<T> extends ArrayBlockingQueue<T> {
+public class BufferQueue<Packet> extends ArrayBlockingQueue<Packet> {
 
     public BufferQueue(int capacity) {
         super(capacity);
