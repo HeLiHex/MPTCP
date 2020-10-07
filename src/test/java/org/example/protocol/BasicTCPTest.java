18c8670607cc716b13ceee662ffb6c8c877ec1d8
Henrik Libeck Hexeberg <henrik.lh@hotmail.com>
2020-10-07 14:01:48 +0200

    ack without sequences has been added

--- src/test/java/org/example/protocol/BasicTCPTest.java
+++ src/test/java/org/example/protocol/BasicTCPTest.java
@@ -1,6 +1,7 @@
 package org.example.protocol;
 
 import org.example.data.Packet;
+import org.example.network.Router;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -50,7 +51,50 @@ public class BasicTCPTest {
         client.send(packet);
 
         try {
-            sleep(1000);
+            sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(server.receive(), packet);
+
+    }
+
+
+    @Test
+    public void connectThenSendMsgOverMultipleNodesLineWorksTest(){
+        BasicTCP client = new BasicTCP(RANDOM_GENERATOR);
+        BasicTCP server = new BasicTCP(RANDOM_GENERATOR);
+        Router r1 = new Router(100, RANDOM_GENERATOR);
+        Router r2 = new Router(100, RANDOM_GENERATOR);
+        Router r3 = new Router(100, RANDOM_GENERATOR);
+        Router r4 = new Router(100, RANDOM_GENERATOR);
+
+        client.addNeighbour(r1);
+        r1.addNeighbour(r2);
+        r2.addNeighbour(r3);
+        r3.addNeighbour(r4);
+        r4.addNeighbour(server);
+
+        r1.start();
+        r2.start();
+        r3.start();
+        r4.start();
+        server.start();
+
+        client.connect(server);
+
+        String msg = "hello på do!";
+        Packet packet = new Packet.PacketBuilder()
+                .withOrigin(client)
+                .withMsg(msg)
+                .withDestination(server)
+                .build();
+
+        client.send(packet);
+
+        try {
+            sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
