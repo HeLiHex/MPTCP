a52b1ce4583e356eb288cd92ed138ed27d76c355
Henrik Libeck Hexeberg <henrik.lh@hotmail.com>
2020-10-07 14:31:01 +0200

    BufferQueue is now generic

--- src/main/java/org/example/data/BufferQueue.java
+++ src/main/java/org/example/data/BufferQueue.java
@@ -2,7 +2,7 @@ package org.example.data;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class BufferQueue<Packet> extends ArrayBlockingQueue<Packet> {
+public class BufferQueue<T> extends ArrayBlockingQueue<T> {
 
     public BufferQueue(int capacity) {
         super(capacity);
