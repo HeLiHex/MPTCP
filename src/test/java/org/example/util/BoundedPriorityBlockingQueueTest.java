b4dd52327de4e03c82260096d25adbcd4c7e2e2d
henrik <henrik.lh@hotmail.com>
2021-01-07 12:04:14 +0100

    test BoundedPriorityBlockingQueue.isFull() method

--- src/test/java/org/example/util/BoundedPriorityBlockingQueueTest.java
+++ src/test/java/org/example/util/BoundedPriorityBlockingQueueTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 
 public class BoundedPriorityBlockingQueueTest {
 
-
     @Test
     public void offerInsertsOrdered(){
         BoundedPriorityBlockingQueue<Packet> boundedPriorityBlockingQueue =
@@ -29,13 +28,11 @@ public class BoundedPriorityBlockingQueueTest {
                 .withSequenceNumber(5)
                 .build();
 
-
         boundedPriorityBlockingQueue.offer(three);
         boundedPriorityBlockingQueue.offer(four);
         boundedPriorityBlockingQueue.offer(one);
         boundedPriorityBlockingQueue.offer(five);
         boundedPriorityBlockingQueue.offer(two);
-
 
         Assert.assertEquals(one, boundedPriorityBlockingQueue.poll());
         Assert.assertEquals(two, boundedPriorityBlockingQueue.poll());
@@ -43,6 +40,63 @@ public class BoundedPriorityBlockingQueueTest {
         Assert.assertEquals(four, boundedPriorityBlockingQueue.poll());
         Assert.assertEquals(five, boundedPriorityBlockingQueue.poll());
         Assert.assertEquals(null, boundedPriorityBlockingQueue.poll());
+    }
+
+    @Test
+    public void isFullWorksReturnsTrueIfFullTest(){
+        BoundedPriorityBlockingQueue<Packet> boundedPriorityBlockingQueue =
+                new BoundedPriorityBlockingQueue<>(5, (packet, t1) -> packet.getSequenceNumber() - t1.getSequenceNumber());
+
+        Packet one = new PacketBuilder()
+                .withSequenceNumber(1)
+                .build();
+        Packet two = new PacketBuilder()
+                .withSequenceNumber(2)
+                .build();
+        Packet three = new PacketBuilder()
+                .withSequenceNumber(3)
+                .build();
+        Packet four = new PacketBuilder()
+                .withSequenceNumber(4)
+                .build();
+        Packet five = new PacketBuilder()
+                .withSequenceNumber(5)
+                .build();
+
+        boundedPriorityBlockingQueue.offer(one);
+        boundedPriorityBlockingQueue.offer(two);
+        boundedPriorityBlockingQueue.offer(three);
+        boundedPriorityBlockingQueue.offer(four);
+        boundedPriorityBlockingQueue.offer(five);
+
+        Assert.assertTrue(boundedPriorityBlockingQueue.isFull());
+    }
+
+
+    @Test
+    public void isFullWorksReturnsFalseIfNotFullTest(){
+        BoundedPriorityBlockingQueue<Packet> boundedPriorityBlockingQueue =
+                new BoundedPriorityBlockingQueue<>(5, (packet, t1) -> packet.getSequenceNumber() - t1.getSequenceNumber());
+
+        Packet one = new PacketBuilder()
+                .withSequenceNumber(1)
+                .build();
+        Packet two = new PacketBuilder()
+                .withSequenceNumber(2)
+                .build();
+        Packet three = new PacketBuilder()
+                .withSequenceNumber(3)
+                .build();
+        Packet four = new PacketBuilder()
+                .withSequenceNumber(4)
+                .build();
+
+        boundedPriorityBlockingQueue.offer(one);
+        boundedPriorityBlockingQueue.offer(two);
+        boundedPriorityBlockingQueue.offer(three);
+        boundedPriorityBlockingQueue.offer(four);
+
+        Assert.assertFalse(boundedPriorityBlockingQueue.isFull());
     }
 
 }
