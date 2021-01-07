4678dae655efb79ea196c87d39d2bd1e8804e69c
henrik <henrik.lh@hotmail.com>
2021-01-07 16:33:56 +0100

    change variable name to lowercase

--- src/main/java/org/example/util/WaitingPacket.java
+++ src/main/java/org/example/util/WaitingPacket.java
@@ -6,17 +6,17 @@ public class WaitingPacket {
 
     private Packet packet;
     private int timeout;
-    private final int TIMEOUT_DURATION;
+    private final int timeoutDuration;
 
     public WaitingPacket(Packet packet, int timeout) {
         this.packet = packet;
-        this.TIMEOUT_DURATION = timeout;
+        this.timeoutDuration = timeout;
         this.timeout = timeout;
     }
 
     public void restart(){
         if (!timeoutFinished()) throw new IllegalStateException("can't restart unfinished timer");
-        this.timeout = TIMEOUT_DURATION;
+        this.timeout = timeoutDuration;
     }
 
     public boolean timeoutFinished(){
