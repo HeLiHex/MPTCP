6f924774f61f301936c6535c97cca03018ad4006
henrik <henrik.lh@hotmail.com>
2021-01-08 15:21:40 +0100

    add javedocs to Payload interface

--- src/main/java/org/example/data/Payload.java
+++ src/main/java/org/example/data/Payload.java
@@ -2,15 +2,35 @@ package org.example.data;
 
 public interface Payload {
 
+    /**
+     * A method that returns the size of the packet.
+     * The size of an object is abstract and must be implemented according to what type
+     * Payload is created.
+     *
+     * @return the size of the packet
+     */
     int size();
 
-    @Override
+    /**
+     * A method that returns a String representation of the Payload
+     *
+     * @return String representation.
+     */
     String toString();
 
-    @Override
+    /**
+     * A method to check if two object is equal or not
+     *
+     * @param obj to be checked against
+     * @return true if this is considered equal to given object
+     */
     boolean equals(Object obj);
 
-    @Override
+    /**
+     * A method that creates a hash number of this object
+     *
+     * @return Integer hash of the object
+     */
     int hashCode();
 
 }
