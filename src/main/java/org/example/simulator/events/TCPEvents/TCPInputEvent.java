59375568e66d9ec33b53231f7376f335391c9716
henrik <henrik.lh@hotmail.com>
2021-01-26 12:16:10 +0100

    created TCPInputEvent

---
+++ src/main/java/org/example/simulator/events/TCPEvents/TCPInputEvent.java
@@ -0,0 +1,35 @@
+package org.example.simulator.events.TCPEvents;
+
+import org.example.protocol.AbstractTCP;
+import org.example.protocol.TCP;
+import org.example.simulator.events.Event;
+
+import java.time.Instant;
+import java.util.Queue;
+
+public class TCPInputEvent extends Event {
+
+    private final TCP tcp;
+
+    public TCPInputEvent(Instant instant, TCP tcp) {
+        super(instant);
+        if (tcp == null) throw new IllegalArgumentException("given TCP can not be null");
+        this.tcp = tcp;
+    }
+
+    public TCPInputEvent(TCP tcp) {
+        super();
+        if (tcp == null) throw new IllegalArgumentException("given TCP can not be null");
+        this.tcp = tcp;
+    }
+
+    @Override
+    public void run() {
+        ((AbstractTCP)this.tcp).handleIncoming();
+    }
+
+    @Override
+    public void generateNextEvent(Queue<Event> events) {
+        //todo - add next event
+    }
+}
