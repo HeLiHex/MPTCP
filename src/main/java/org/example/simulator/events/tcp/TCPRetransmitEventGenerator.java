ad4eebe79d0817b2b4cbb45b2aa9224270385c48
henrik <henrik.lh@hotmail.com>
2021-03-03 12:33:02 +0100

    commenting out a logger

--- src/main/java/org/example/simulator/events/tcp/TCPRetransmitEventGenerator.java
+++ src/main/java/org/example/simulator/events/tcp/TCPRetransmitEventGenerator.java
@@ -27,7 +27,7 @@ public class TCPRetransmitEventGenerator extends EventGenerator {
         if (!this.tcp.isConnected()) return;
 
         if (this.tcp.inSendingWindow(this.packet) && this.tcp.packetIsWaiting(this.packet)) {
-            Logger.getLogger(this.getClass().getSimpleName()).log(Level.INFO, () -> "Retransmit packet: " + this.packet);
+            //Logger.getLogger(this.getClass().getSimpleName()).log(Level.INFO, () -> "Retransmit packet: " + this.packet);
             Statistics.packetRetransmit();
             events.add(new RouteEvent(this.tcp, packet));
             events.add(new TCPRetransmitEventGenerator(this.tcp, packet));
