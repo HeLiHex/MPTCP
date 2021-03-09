1fb6c678bc5ea741cc5b73e2830f7d34753e693b
henrik <henrik.lh@hotmail.com>
2021-03-09 10:11:51 +0100

    add resizing of windows

--- src/main/java/org/example/protocol/window/sending/SlidingWindow.java
+++ src/main/java/org/example/protocol/window/sending/SlidingWindow.java
@@ -12,12 +12,14 @@ import java.util.Comparator;
 public class SlidingWindow extends Window implements SendingWindow, BoundedQueue<Packet> {
 
     private final BoundedQueue<Packet> window;
-    private final int defaultCongestionSize;
+    private static final int DEFAULT_CONGESTION_WINDOW_SIZE = 1;
+    private final int receiverWindowSize;
+    private int dupAckCount = 0;
 
-    public SlidingWindow(int windowSize, Connection connection, Comparator comparator) {
+    public SlidingWindow(int receiverWindowSize, Connection connection, Comparator comparator) {
         super(1000000, connection, comparator);
-        this.window = new BoundedPriorityBlockingQueue<>(windowSize, comparator);
-        this.defaultCongestionSize = 1;
+        this.window = new BoundedPriorityBlockingQueue<>(DEFAULT_CONGESTION_WINDOW_SIZE, comparator);
+        this.receiverWindowSize = receiverWindowSize;
     }
 
     @Override
@@ -28,9 +30,10 @@ public class SlidingWindow extends Window implements SendingWindow, BoundedQueue
     @Override
     public void ackReceived(Packet ack) {
         int ackIndex = this.sendingPacketIndex(ack);
+
         for (int i = 0; i <= ackIndex; i++) {
-            System.out.println("poll");
             this.window.poll();
+            this.increase();
         }
         this.connection.update(ack);
     }
@@ -47,23 +50,30 @@ public class SlidingWindow extends Window implements SendingWindow, BoundedQueue
 
     @Override
     public boolean canRetransmit(Packet packet) {
-        return this.window.contains(packet);
+        if (this.window.contains(packet)){
+            if (this.sendingPacketIndex(packet) == 0) this.decrease();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void increase() {
+        if (this.getWindowSize() >= this.receiverWindowSize) return;
         this.window.resize(this.getWindowSize() + 1);
     }
 
     @Override
     public void decrease() {
-        int curWindowSize = this.getWindowSize();
-        int newWindowSize = (int)Math.ceil(curWindowSize/2);
-        if (newWindowSize == 0) newWindowSize = this.defaultCongestionSize;
-
-        for (int i = 0; i < curWindowSize - newWindowSize; i++) {
+        int newWindowSize = Math.max((int) Math.ceil(this.getWindowSize() / 2), DEFAULT_CONGESTION_WINDOW_SIZE);
+        for (int i = 0; i < this.getWindowSize() - newWindowSize; i++) {
             this.offer(this.window.poll());
         }
         this.window.resize(newWindowSize);
+    }
+
+    @Override
+    public int getWindowSize() {
+        return this.window.size();
     }
 }
