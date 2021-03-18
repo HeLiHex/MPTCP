7d36b47a1ff8acff7994ba34a6e202efb3743c99
HeLiHex <henrik.lh@hotmail.com>
2021-03-18 15:13:12 +0100

    minor bug fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
+import org.knowm.xchart.internal.chartpart.Chart;
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -17,7 +18,7 @@ public class Main {
         XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
 
         // Show it
-        SwingWrapper<XYChart> sw = new SwingWrapper(chart);
+        SwingWrapper<XYChart> sw = new SwingWrapper<>(chart);
         sw.displayChart();
 
         // Save it
