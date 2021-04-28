3643bc2525da030ff4a1957c276a1e69928c44d9
HeLiHex <henrik.lh@hotmail.com>
2021-04-28 12:17:51 +0200

    comment out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         double[] xData = new double[]{0.0, 1.0, 2.0};
         double[] yData = new double[]{2.0, 1.0, 0.0};
 
@@ -29,5 +30,7 @@ public class Main {
         } catch (IOException e) {
             Logger.getLogger(Main.class.getSimpleName()).log(Level.WARNING, "chart fail");
         }
+
+         */
     }
 }
