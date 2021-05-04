6777232132404692883a7a2b6e6dbb4a684ed238
HeLiHex <henrik.lh@hotmail.com>
2021-05-04 16:32:49 +0200

    add new test

--- src/test/java/org/example/simulator/statistics/TCPStatsTest.java
+++ src/test/java/org/example/simulator/statistics/TCPStatsTest.java
@@ -14,6 +14,7 @@ import org.example.simulator.events.tcp.TCPConnectEvent;
 import org.example.util.Util;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TCPStatsTest {
@@ -157,6 +158,112 @@ public class TCPStatsTest {
         Assert.assertTrue(r2.inputBufferIsEmpty());
         Assert.assertTrue(r3.inputBufferIsEmpty());
 
+
+        for (int i = 1; i <= numPacketsToSend; i++) {
+            Message msg = new Message("test " + i);
+            Packet received = server.receive();
+            Assert.assertNotNull(received);
+            Assert.assertEquals("iteration " + i, received.getPayload(), msg);
+        }
+        Assert.assertNull(server.receive());
+
+        eventHandler.printStatistics();
+
+        //receiver
+        for (TCPStats stat: server.getTcpStats()) {
+            System.out.println(stat.toString());
+            stat.createArrivalChart();
+            stat.createDepartureChart();
+            stat.createInterArrivalChart();
+            stat.createTimeInSystemChart();
+        }
+
+        //sender
+        for (TCPStats stat: client.getTcpStats()) {
+            System.out.println(stat.toString());
+            stat.createCWNDChart();
+        }
+
+    }
+
+
+
+
+
+    @Test
+    @Ignore
+    public void MPTCPRealistic2HomogeneousDisconnectedFlowsWithDifferentTrafficParameters() {
+        MPTCP client = new MPTCP(2, 20);
+
+        Routable r11 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("A1")).build();
+        Routable r12 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("A2")).build();
+        Routable r13 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("A3")).build();
+        Routable r14 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("A4")).build();
+
+        Routable r21 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("B1")).build();
+        Routable r22 = new Router.RouterBuilder().withAverageQueueUtilization(0.85).withAddress(new SimpleAddress("B2")).build();
+        Routable r23 = new Router.RouterBuilder().withAverageQueueUtilization(0.85).withAddress(new SimpleAddress("B3")).build();
+        Routable r24 = new Router.RouterBuilder().withAverageQueueUtilization(0.8).withAddress(new SimpleAddress("B4")).build();
+
+        MPTCP server = new MPTCP(2, 20);
+
+        //path one
+        new Channel.ChannelBuilder().withNoiseTolerance(2.8).build(client, r11);
+        new Channel.ChannelBuilder().build(r11, r12);
+        new Channel.ChannelBuilder().build(r12, r13);
+        new Channel.ChannelBuilder().build(r13, r14);
+        new Channel.ChannelBuilder().withNoiseTolerance(2.8).build(r14, server);
+
+        //path two
+        new Channel.ChannelBuilder().withNoiseTolerance(2.8).build(client, r21);
+        new Channel.ChannelBuilder().build(r21, r22);
+        new Channel.ChannelBuilder().build(r22, r23);
+        new Channel.ChannelBuilder().build(r23, r24);
+        new Channel.ChannelBuilder().withNoiseTolerance(2.8).build(r24, server);
+
+        client.updateRoutingTable();
+
+        r11.updateRoutingTable();
+        r12.updateRoutingTable();
+        r13.updateRoutingTable();
+        r14.updateRoutingTable();
+
+        r21.updateRoutingTable();
+        r22.updateRoutingTable();
+        r23.updateRoutingTable();
+        r24.updateRoutingTable();
+
+        server.updateRoutingTable();
+
+        EventHandler eventHandler = new EventHandler();
+        eventHandler.addEvent(new TCPConnectEvent(client, server));
+        eventHandler.run();
+        System.out.println("connected");
+
+        Assert.assertTrue(client.getSubflows()[0].isConnected());
+        Assert.assertTrue(client.getSubflows()[1].isConnected());
+
+        int numPacketsToSend = 10000;
+
+        for (int i = 1; i <= numPacketsToSend; i++) {
+            Message msg = new Message("test " + i);
+            client.send(msg);
+        }
+        eventHandler.addEvent(new RunTCPEvent(client));
+        eventHandler.run();
+
+        Assert.assertTrue("client still has packets to send", client.outputBufferIsEmpty());
+        Assert.assertTrue(server.outputBufferIsEmpty());
+        Assert.assertTrue(client.inputBufferIsEmpty());
+        Assert.assertTrue(server.inputBufferIsEmpty());
+        Assert.assertTrue(r11.inputBufferIsEmpty());
+        Assert.assertTrue(r12.inputBufferIsEmpty());
+        Assert.assertTrue(r13.inputBufferIsEmpty());
+        Assert.assertTrue(r14.inputBufferIsEmpty());
+        Assert.assertTrue(r21.inputBufferIsEmpty());
+        Assert.assertTrue(r22.inputBufferIsEmpty());
+        Assert.assertTrue(r23.inputBufferIsEmpty());
+        Assert.assertTrue(r24.inputBufferIsEmpty());
 
         for (int i = 1; i <= numPacketsToSend; i++) {
             Message msg = new Message("test " + i);
