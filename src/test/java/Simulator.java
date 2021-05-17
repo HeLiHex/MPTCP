7c3aa16697a1d12d39d726ddf9a955ddf1556132
HeLiHex <henrik.lh@hotmail.com>
2021-05-17 21:28:15 +0200

    add test

--- src/test/java/Simulator.java
+++ src/test/java/Simulator.java
@@ -5,8 +5,10 @@ import org.example.network.Routable;
 import org.example.network.Router;
 import org.example.network.address.SimpleAddress;
 import org.example.protocol.ClassicTCP;
+import org.example.protocol.MPTCP;
 import org.example.protocol.TCP;
 import org.example.simulator.EventHandler;
+import org.example.simulator.events.tcp.RunTCPEvent;
 import org.example.simulator.events.tcp.TCPConnectEvent;
 import org.example.simulator.statistics.TCPStats;
 import org.example.util.Util;
@@ -21,7 +23,7 @@ public class Simulator {
     @Before
     public void setUp(){
         Util.resetTime();
-        Util.setSeed(1337);
+        Util.setSeed(1);
     }
 
     private void numPacketsToSend(TCP tcpToSend, int numPacketsToSend){
@@ -50,17 +52,17 @@ public class Simulator {
     @Test
     public void shortPathLowLoss(){
         ClassicTCP client = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Client-ShortPathLowLoss")).build();
-        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
         Routable r2 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
         Routable r3 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
-        Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 4")).build();
+        Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 4")).build();
         ClassicTCP server = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Server-ShortPathLowLoss")).build();
 
-        new Channel.ChannelBuilder().build(client, r1);
-        new Channel.ChannelBuilder().build(r1, r2);
-        new Channel.ChannelBuilder().withLoss(0.001).build(r2, r3);
-        new Channel.ChannelBuilder().build(r3, r4);
-        new Channel.ChannelBuilder().build(r4, server);
+        new Channel.ChannelBuilder().withCost(1).build(client, r1);
+        new Channel.ChannelBuilder().withCost(10).build(r1, r2);
+        new Channel.ChannelBuilder().withCost(10).withLoss(0.001).build(r2, r3);
+        new Channel.ChannelBuilder().withCost(10).build(r3, r4);
+        new Channel.ChannelBuilder().withCost(1).build(r4, server);
 
         client.updateRoutingTable();
         r1.updateRoutingTable();
@@ -98,17 +100,18 @@ public class Simulator {
     @Test
     public void shortPathHighLoss(){
         ClassicTCP client = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Client-ShortPathHighLoss")).build();
-        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
         Routable r2 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
         Routable r3 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
-        Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 4")).build();
+        Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 4")).build();
         ClassicTCP server = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Server-ShortPathHighLoss")).build();
 
-        new Channel.ChannelBuilder().build(client, r1);
-        new Channel.ChannelBuilder().build(r1, r2);
-        new Channel.ChannelBuilder().withLoss(0.01).build(r2, r3);
-        new Channel.ChannelBuilder().build(r3, r4);
-        new Channel.ChannelBuilder().build(r4, server);
+
+        new Channel.ChannelBuilder().withCost(1).build(client, r1);
+        new Channel.ChannelBuilder().withCost(10).build(r1, r2);
+        new Channel.ChannelBuilder().withCost(10).withLoss(0.01).build(r2, r3);
+        new Channel.ChannelBuilder().withCost(10).build(r3, r4);
+        new Channel.ChannelBuilder().withCost(1).build(r4, server);
 
         client.updateRoutingTable();
         r1.updateRoutingTable();
@@ -140,12 +143,13 @@ public class Simulator {
 
         System.out.println(server.getStats().toString());
         server.getStats().createCWNDChart();
+        server.getStats().createDepartureChart();
     }
 
     @Test
     public void longPathLowLoss(){
         ClassicTCP client = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Client-LongPathLowLoss")).build();
-        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
         Routable r2 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
         Routable r3 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
         Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 4")).build();
@@ -154,20 +158,20 @@ public class Simulator {
         Routable r7 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 7")).build();
         Routable r8 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 8")).build();
         Routable r9 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 9")).build();
-        Routable r10 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 10")).build();
+        Routable r10 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 10")).build();
         ClassicTCP server = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Server-LongPathLowLoss")).build();
 
-        new Channel.ChannelBuilder().build(client, r1);
-        new Channel.ChannelBuilder().build(r1, r2);
-        new Channel.ChannelBuilder().build(r2, r3);
-        new Channel.ChannelBuilder().build(r3, r4);
-        new Channel.ChannelBuilder().build(r4, r5);
-        new Channel.ChannelBuilder().withLoss(0.001).build(r5, r6);
-        new Channel.ChannelBuilder().build(r6, r7);
-        new Channel.ChannelBuilder().build(r7, r8);
-        new Channel.ChannelBuilder().build(r8, r9);
-        new Channel.ChannelBuilder().build(r9, r10);
-        new Channel.ChannelBuilder().build(r10, server);
+        new Channel.ChannelBuilder().withCost(1).build(client, r1);
+        new Channel.ChannelBuilder().withCost(10).build(r1, r2);
+        new Channel.ChannelBuilder().withCost(50).build(r2, r3);
+        new Channel.ChannelBuilder().withCost(50).build(r3, r4);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.001).build(r4, r5);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.001).build(r5, r6);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.001).build(r6, r7);
+        new Channel.ChannelBuilder().withCost(50).build(r7, r8);
+        new Channel.ChannelBuilder().withCost(50).build(r8, r9);
+        new Channel.ChannelBuilder().withCost(10).build(r9, r10);
+        new Channel.ChannelBuilder().withCost(1).build(r10, server);
 
         client.updateRoutingTable();
         r1.updateRoutingTable();
@@ -210,7 +214,7 @@ public class Simulator {
     @Test
     public void longPathHighLoss(){
         ClassicTCP client = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Client-LongPathHighLoss")).build();
-        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r1 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
         Routable r2 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
         Routable r3 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
         Routable r4 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 4")).build();
@@ -219,20 +223,20 @@ public class Simulator {
         Routable r7 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 7")).build();
         Routable r8 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 8")).build();
         Routable r9 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 9")).build();
-        Routable r10 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 10")).build();
+        Routable r10 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 10")).build();
         ClassicTCP server = new ClassicTCP.ClassicTCPBuilder().withReceivingWindowCapacity(30).setReno().withAddress(new SimpleAddress("Server-LongPathHighLoss")).build();
 
-        new Channel.ChannelBuilder().build(client, r1);
-        new Channel.ChannelBuilder().build(r1, r2);
-        new Channel.ChannelBuilder().build(r2, r3);
-        new Channel.ChannelBuilder().build(r3, r4);
-        new Channel.ChannelBuilder().build(r4, r5);
-        new Channel.ChannelBuilder().withLoss(0.01).build(r5, r6);
-        new Channel.ChannelBuilder().build(r6, r7);
-        new Channel.ChannelBuilder().build(r7, r8);
-        new Channel.ChannelBuilder().build(r8, r9);
-        new Channel.ChannelBuilder().build(r9, r10);
-        new Channel.ChannelBuilder().build(r10, server);
+        new Channel.ChannelBuilder().withCost(1).build(client, r1);
+        new Channel.ChannelBuilder().withCost(10).build(r1, r2);
+        new Channel.ChannelBuilder().withCost(50).build(r2, r3);
+        new Channel.ChannelBuilder().withCost(50).build(r3, r4);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.001).build(r4, r5);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.01).build(r5, r6);
+        new Channel.ChannelBuilder().withCost(50).withLoss(0.001).build(r6, r7);
+        new Channel.ChannelBuilder().withCost(50).build(r7, r8);
+        new Channel.ChannelBuilder().withCost(50).build(r8, r9);
+        new Channel.ChannelBuilder().withCost(10).build(r9, r10);
+        new Channel.ChannelBuilder().withCost(1).build(r10, server);
 
         client.updateRoutingTable();
         r1.updateRoutingTable();
@@ -270,6 +274,90 @@ public class Simulator {
 
         System.out.println(server.getStats().toString());
         server.getStats().createCWNDChart();
+    }
+
+
+    private void getStats(MPTCP sender, MPTCP receiver){
+        //receiver
+        for (TCPStats stat: receiver.getTcpStats()) {
+            System.out.println(stat.toString());
+            stat.createArrivalChart();
+            stat.createDepartureChart();
+            stat.createInterArrivalChart();
+            stat.createTimeInSystemChart();
+            stat.createNumberOfPacketsInSystemChart();
+        }
+
+        //sender
+        for (TCPStats stat: sender.getTcpStats()) {
+            System.out.println(stat.toString());
+            stat.createCWNDChart();
+        }
+    }
+
+    @Test
+    public void MPTCPFloodWithPacketsInOrderShouldWorkTest() {
+        MPTCP client = new MPTCP.MPTCPBuilder().withNumberOfSubflows(2).withReceivingWindowCapacity(30).withAddress(new SimpleAddress("MPTCP-Client")).build();
+
+        Routable r11 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r12= new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
+        Routable r13 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
+        Routable r14 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 4")).build();
+
+        Routable r21 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 1")).build();
+        Routable r22 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 2")).build();
+        Routable r23 = new Router.RouterBuilder().withAverageQueueUtilization(0.98).withAddress(new SimpleAddress("Router 3")).build();
+        Routable r24 = new Router.RouterBuilder().withAverageQueueUtilization(0.5).withAddress(new SimpleAddress("Router 4")).build();
+
+        MPTCP server = new MPTCP.MPTCPBuilder().withNumberOfSubflows(2).withReceivingWindowCapacity(30).withAddress(new SimpleAddress("MPTCP-Server")).build();
+
+        //path one
+        new Channel.ChannelBuilder().withCost(1).build(client, r11);
+        new Channel.ChannelBuilder().withCost(10).build(r11, r12);
+        new Channel.ChannelBuilder().withCost(10).withLoss(0.00).build(r12, r13);
+        new Channel.ChannelBuilder().withCost(10).build(r13, r14);
+        new Channel.ChannelBuilder().withCost(1).build(r14, server);
+
+        //path two
+        new Channel.ChannelBuilder().withCost(1).build(client, r21);
+        new Channel.ChannelBuilder().withCost(10).build(r21, r22);
+        new Channel.ChannelBuilder().withCost(10).withLoss(0.00).build(r22, r23);
+        new Channel.ChannelBuilder().withCost(10).build(r23, r24);
+        new Channel.ChannelBuilder().withCost(1).build(r24, server);
+
+        client.updateRoutingTable();
+        r11.updateRoutingTable();
+        r12.updateRoutingTable();
+        r13.updateRoutingTable();
+        r14.updateRoutingTable();
+        r21.updateRoutingTable();
+        r22.updateRoutingTable();
+        r23.updateRoutingTable();
+        r24.updateRoutingTable();
+        server.updateRoutingTable();
+
+        this.numPacketsToSend(server, this.numPacketsToSend);
+        EventHandler eventHandler = this.connectAndRun(server, client);
+
+        Assert.assertTrue("client still has packets to send", client.outputBufferIsEmpty());
+        Assert.assertTrue(server.outputBufferIsEmpty());
+        Assert.assertTrue(client.inputBufferIsEmpty());
+        Assert.assertTrue(server.inputBufferIsEmpty());
+        Assert.assertTrue(r11.inputBufferIsEmpty());
+        Assert.assertTrue(r12.inputBufferIsEmpty());
+        Assert.assertTrue(r13.inputBufferIsEmpty());
+        Assert.assertTrue(r14.inputBufferIsEmpty());
+        Assert.assertTrue(r21.inputBufferIsEmpty());
+        Assert.assertTrue(r22.inputBufferIsEmpty());
+        Assert.assertTrue(r23.inputBufferIsEmpty());
+        Assert.assertTrue(r24.inputBufferIsEmpty());
+
+        this.allReceived(client, this.numPacketsToSend);
+
+        Assert.assertNull(server.receive());
+        eventHandler.printStatistics();
+        this.getStats(server, client);
+
     }
 
 }
